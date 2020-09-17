a0a57c04b068257bae748e575d8f9a0a28d2c2f4
Wesley <69126784+C-FWES@users.noreply.github.com>
2020-09-17 17:51:45 -0400

    Update TestStack.java

--- TestStack.java
+++ TestStack.java
@@ -1,9 +1,6 @@
-package Stack;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class TestStack<E> {
     private List<E> list = new ArrayList<>();
